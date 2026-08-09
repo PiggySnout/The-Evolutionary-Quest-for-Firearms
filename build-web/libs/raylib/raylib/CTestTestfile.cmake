5f5917238add4729e9f78026600827ebae7c064c
Joseph Todd <josephustoddus@gmail.com>
2026-08-09 19:20:30 +0300

    Delete build-web directory

--- build-web/libs/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/josep/Desktop/Programming/Game Jam/libs/raylib/src
-# Build directory: C:/Users/josep/Desktop/Programming/Game Jam/build-web/libs/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
