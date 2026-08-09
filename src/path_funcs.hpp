3c520da02ffaed09c800eb506b20025bef9c20b8
Joseph Todd <josephustoddus@gmail.com>
2026-08-09 19:19:23 +0300

    Update asset path retrieval for Windows and Apple

--- src/path_funcs.hpp
+++ src/path_funcs.hpp
@@ -2,17 +2,39 @@
 #include "raylib.h"
 #include <string>
 
+#ifdef _WIN32
+
 inline const std::string GetSoundPath(std::string a) {
-    static const std::string path =  GetApplicationDirectory() + "assets/sound/";
+    static const std::string path = std::string(GetApplicationDirectory()) + "../../assets/sound/";
     return path + a;
 }
 
 inline const std::string GetDataPath(std::string a) {
-    static const std::string path = GetApplicationDirectory() + "assets/Data/";
+    static const std::string path = std::string(GetApplicationDirectory()) + "../../assets/data/";
     return path + a;
 }
 
 inline const std::string GetAssetsPath(std::string a) {
-    static const std::string path = GetApplicationDirectory() + "assets/textures/";
+    static const std::string path = std::string(GetApplicationDirectory()) + "../../assets/textures/";
     return path + a;
 }
+
+#endif
+#ifdef __APPLE__
+
+inline const std::string GetSoundPath(std::string a) {
+    static const std::string path = std::string(GetApplicationDirectory()) + "../Resources/assets/sound/";
+    return path + a;
+}
+
+inline const std::string GetDataPath(std::string a) {
+    static const std::string path = std::string(GetApplicationDirectory()) + "../Resources/assets/data/";
+    return path + a;
+}
+
+inline const std::string GetAssetsPath(std::string a) {
+    static const std::string path = std::string(GetApplicationDirectory()) + "../Resources/assets/textures/";
+    return path + a;
+}
+
+#endif
